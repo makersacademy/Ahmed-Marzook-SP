6a291c4c2097ba788261485eb1bfb1d33e3ab696
Ahmed Marzook <m4marzook@gmail.com>
2020-07-06 11:44:45 +0100

    Spring Boot Backend
    Init the Spring boot backend to use java 11

---
+++ quiz_managment_backend/src/main/java/com/quix/webbiskoolsltd/QuizManagmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quix.webbiskoolsltd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizManagmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizManagmentBackendApplication.class, args);
+	}
+
+}
