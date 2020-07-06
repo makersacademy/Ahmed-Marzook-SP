739a08c33e2d48895da24e7ef5ac1350127ff284
Ahmed Marzook <m4marzook@gmail.com>
2020-07-06 12:35:13 +0100

    typo renamed

--- quiz-managment-backend/src/main/java/com/quiz/webbiskoolsltd/QuizManagmentBackendApplication.java
+++ quiz-managment-backend/src/main/java/com/quiz/webbiskoolsltd/QuizManagmentBackendApplication.java
@@ -1,4 +1,4 @@
-package com.quix.webbiskoolsltd;
+package com.quiz.webbiskoolsltd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
