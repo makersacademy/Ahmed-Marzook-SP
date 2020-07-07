dfefb0ab4ca3914fd615318cf5a606ca6a1a3eae
Ahmed Marzook <m4marzook@gmail.com>
2020-07-07 10:47:15 +0100

    Header and Footer DOne

--- quiz-manager-frontend/src/app/app.module.ts
+++ quiz-manager-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './layout/login/login.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
